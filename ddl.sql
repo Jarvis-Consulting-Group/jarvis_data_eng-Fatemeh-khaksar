f957fa69da1fa3bc6c805a90ace5e85d54431dfa
Fatemeh Khaksar <fatemeh.khaksarboldaji@gmail.com>
2023-03-14 16:06:33 +0000

    monitoring agent

--- ddl.sql
+++
@@ -1,6 +0,0 @@
- id |                                   hostname                                   | cpu_number | cpu_architecture |           cpu_model            | cpu_mhz | l2_cache |      timestamp      | total_mem 
-----+------------------------------------------------------------------------------+------------+------------------+--------------------------------+---------+----------+---------------------+-----------
-  1 | jrvs-remote-desktop-centos7-6.us-central1-a.c.spry-framework-236416.internal |          1 | x86_64           | Intel(R) Xeon(R) CPU @ 2.30GHz |    2300 |      256 | 2019-05-29 17:49:53 |    601324
-  2 | noe2                                                                         |          1 | x86_64           | Intel(R) Xeon(R) CPU @ 2.30GHz |    2300 |      256 | 2019-05-29 17:49:53 |    601324
-(2 rows)
-
